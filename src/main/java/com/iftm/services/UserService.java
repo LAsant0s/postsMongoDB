316df43ebe8c2c7c2bdfdc6b8475dfa6678fba22
luansantos <luan.assis05.santos@gmail.com>
2021-09-24 19:33:18 -0300

    GET User by id

--- src/main/java/com/iftm/services/UserService.java
+++ src/main/java/com/iftm/services/UserService.java
@@ -1,6 +1,7 @@
 package com.iftm.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 import com.iftm.models.dto.UserDTO;
 import com.iftm.models.entities.User;
 import com.iftm.repositories.UserRepository;
+import com.iftm.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -19,5 +21,12 @@ public class UserService {
 	public List<UserDTO> findAll() {
 		List<User> list = repository.findAll();
 		return list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+	}
+	
+	public UserDTO findById(String id) {
+		Optional<User> result = repository.findById(id);
+		User entity = result.orElseThrow(() -> new ResourceNotFoundException("Recurso não encontrado"));
+		
+		return new UserDTO(entity);
 	}
 }
