bd8e7358f5ba1f12c98a1989f928f129f10c0cf7
luansantos <luan.assis05.santos@gmail.com>
2021-09-24 19:54:17 -0300

    POST user

--- src/main/java/com/iftm/controllers/UserController.java
+++ src/main/java/com/iftm/controllers/UserController.java
@@ -1,13 +1,17 @@
 package com.iftm.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.iftm.models.dto.UserDTO;
 import com.iftm.services.UserService;
@@ -29,6 +33,13 @@ public class UserController {
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		UserDTO dto = service.findById(id);
 		return ResponseEntity.ok().body(dto);
+	}
+	
+	@PostMapping
+	public ResponseEntity<UserDTO> insert(@RequestBody UserDTO dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
 	}
 	
 }
