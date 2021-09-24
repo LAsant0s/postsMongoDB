04413540445f0aeb1bd46610be5fb98708510dfb
luansantos <luan.assis05.santos@gmail.com>
2021-09-24 20:16:57 -0300

    Delete User

--- src/main/java/com/iftm/services/UserService.java
+++ src/main/java/com/iftm/services/UserService.java
@@ -46,6 +46,10 @@ public class UserService {
 		return new UserDTO(entity);
 	}
 	
+	public void delete(String id) {
+		repository.deleteById(id);
+	}
+	
 	private void copyDtoToEntity(UserDTO dto, User entity) {
 		entity.setName(dto.getName());
 		entity.setEmail(dto.getEmail());	
